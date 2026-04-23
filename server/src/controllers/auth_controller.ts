6967d095a5a788e12a50bef3e50d591e58f7dc01
Aryanshu919 <aryanshu919@gmail.com>
2026-04-23 21:54:38 +0530

    final commit

--- server/src/controllers/auth_controller.ts
+++ server/src/controllers/auth_controller.ts
@@ -50,7 +50,7 @@ export const register = async (req: Request, res: Response) => {
   res
     .cookie("auth-cookie", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
@@ -100,7 +100,7 @@ export const login = async (req: Request, res: Response) => {
   res
     .cookie("auth-cookie", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
