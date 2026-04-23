0e01d32c6832803d1728942bcfbf8006d0240531
Aryanshu919 <aryanshu919@gmail.com>
2026-04-23 20:54:23 +0530

    removed dist

--- server/dist/routes/bookmark_routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const bookmark_controller_1 = require("../controllers/bookmark_controller");
-const auth_middleware_1 = require("../middlewares/auth_middleware");
-const bookmarkRouter = (0, express_1.Router)();
-bookmarkRouter.route("/").post(auth_middleware_1.authMiddleware, bookmark_controller_1.bookmarkQuestion);
-exports.default = bookmarkRouter;
